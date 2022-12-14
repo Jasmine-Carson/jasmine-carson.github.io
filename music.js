c7ad1b6e44136a6dbe6fe25d27af3660f3c5c47f
Jasmine Carson <fairy7823@gmail.com>
2022-12-14 16:37:13 -0500

    Update music.js

--- music.js
+++ music.js
@@ -4,6 +4,7 @@
 //class chord contains: root, numeral, inversion, four notes (SATB). initialized with bass note.
 //class note has pitch and octave. default to natural unless accidental is submitted.
 //depth first search
-//get permissible chords. check if permitted by prev chord & next bass note (for 6/4 chords). if none permissible, backtrack.
+//methods needed: get chords. get inversions. check progression. check bass line/chords for 6/4. check fifths/octaves/tones.
+//get permissible chords. determine inversions. check if permitted by prev chord & next bass note (for 6/4 chords). if none permissible, backtrack.
 //build first permissible chord. check for parallel fifths and octaves & frustrated leading tones. if none, move forward.
 //later, try tracking octave gaps b/t upper 3 voices also
